72b2c1a13f246f7fb0a43ae8b4980f2936b7b894
mktintumon <mktintumon@gmail.com>
2022-09-18 15:35:16 +0530

    added

---
+++ 4.Array-String/minEleRotatedSortedArray_2.java
@@ -0,0 +1,28 @@
+class minEleRotatedSortedArray_2 {
+
+    // contains duplicate elements
+
+    public int findMin(int[] nums) {
+        int low = 0;
+        int high = nums.length-1;
+        
+        while(low < high){
+            int mid = low + (high - low) / 2;
+            
+            if(nums[high] > nums[mid]){
+                high = mid;
+            }
+            else if(nums[high] < nums[mid]){
+                low = mid + 1;
+            }
+            else{
+                //equal
+                // ultimately it convergers to low only 
+                // so keep low as it is
+                high--;
+            }
+        }
+        
+        return nums[low];
+    }
+}
