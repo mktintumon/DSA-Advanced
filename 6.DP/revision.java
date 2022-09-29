0f61776158e11daa2ffdb2faeefaa8f19290d65e
mktintumon <mktintumon@gmail.com>
2022-09-29 22:29:54 +0530

    added

--- 6.DP/revision.java
+++ 6.DP/revision.java
@@ -4,10 +4,10 @@ public class revision{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int m = sc.nextInt();
 
-        int[] dp = new int[n+1];
-        // int ans = fibT(n , dp);
-        int ans = climbStairVarJump(n , dp);
+        //int[] dp = new int[n+1];
+        int ans = maxGoldmine(n , m);
         
         System.out.println(ans);
         sc.close();
@@ -79,4 +79,103 @@ public class revision{
 
         return dp[0];
     }  
+
+    public static int minSteps(int n){
+        int[] arr = {3, 3, 0, 2, 1, 2, 4, 2, 0, 0};
+        Integer[] dp = new Integer[n+1]; // default -> null
+        dp[n] = 1; 
+
+        for(int i = n-1 ; i >= 0 ; i--){
+            int min = Integer.MAX_VALUE;
+
+            for(int step=1 ; step <= arr[i] && i+step<=n ; step++){
+                if(dp[i + step] != null){
+                    min = Math.min(min , dp[i + step]);
+                }
+            }
+
+            if(min != Integer.MAX_VALUE){
+                dp[i] = min + 1;
+            }
+            else{
+                dp[i] = null;
+            }
+        }
+
+        System.out.println(Arrays.toString(dp));
+        return dp[0];
+    }
+
+    public static int minCostMazePath(int n , int m){
+        int[][] cost = {
+            {0, 1, 4, 2, 8, 2},
+            {4, 3, 6, 5, 0, 4},
+            {1, 2, 4, 1, 4, 6},
+            {2, 0, 7, 3, 2, 2},
+            {3, 1, 5, 9, 2, 4},
+            {2, 7, 0, 8, 5, 1}
+        };
+
+        int[][] dp = new int[n][m];
+
+        for(int r = n-1; r >= 0 ; r--){
+            for(int c = m-1; c >= 0 ; c--){
+                if(r == n-1 && c == m-1){
+                    dp[r][c] = cost[r][c];
+                }
+                else if(r == n-1){
+                    dp[r][c] = dp[r][c+1] + cost[r][c];
+                }
+                else if(c == m-1){
+                    dp[r][c] = dp[r+1][c] + cost[r][c];
+                }
+                else{
+                    int min = Math.min(dp[r+1][c] , dp[r][c+1]);
+                    dp[r][c] = min + cost[r][c];
+                }
+            }
+        }
+
+        return dp[0][0];
+    }
+
+    public static int maxGoldmine(int n , int m){
+        int[][] gold = {
+            {0, 1, 4, 2, 8, 2},
+            {4, 3, 6, 5, 0, 4},
+            {1, 2, 4, 1, 4, 6},
+            {2, 0, 7, 3, 2, 2},
+            {3, 1, 5, 9, 2, 4},
+            {2, 7, 0, 8, 5, 1}
+        };
+
+        int[][] dp = new int[n][m];
+
+        // direction <-----
+        for(int j = n-1; j >= 0 ; j--){
+            for(int i = 0; i < m; i++) {
+				if(j == n - 1) {
+					dp[i][j] = gold[i][j];
+				} 
+                else if(i == 0) {
+					dp[i][j] = Math.max(dp[i][j + 1], dp[i + 1][j + 1]) + gold[i][j];
+				} 
+                else if(i == m - 1) {
+					dp[i][j] = Math.max(dp[i][j + 1], dp[i - 1][j + 1]) + gold[i][j];
+				} 
+                else {
+                    int max = Math.max(dp[i][j + 1] , Math.max(dp[i + 1][j + 1], dp[i - 1][j + 1]));
+					dp[i][j] =  max + gold[i][j];
+				}
+			}
+        }
+
+        // searching in first column
+        int max = Integer.MIN_VALUE;
+		for(int i = 0; i < m; i++) {
+			max = Math.max(max, dp[i][0]);
+		}
+
+        return max;
+    }
 }
